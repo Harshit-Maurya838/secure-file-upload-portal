771175f7943659b1f76215afe78b380c0979ab58
Harshit-Maurya838 <44harshitmaurya@gmail.com>
2026-02-14 19:55:56 +0530

    feat: Filname Randomize

--- uploads/models.py
+++ uploads/models.py
@@ -1,9 +1,17 @@
+import os
+import uuid
 from django.db import models
 from django_cryptography.fields import encrypt
 
 
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    return os.path.join('uploads/', filename)
+
+
 class UploadedFile(models.Model):
-    file = models.FileField(upload_to='uploads/')
+    file = models.FileField(upload_to=get_file_path)
     description = encrypt(models.CharField(max_length=255, blank=True))
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
